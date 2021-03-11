6353069a5415a7c375408e0a21ac50a08de54c39
Marko Sutic <msuticm@gmail.com>
2021-03-12 00:40:25 +0100

    lead update enabled

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import lead_list, detail_view, lead_create
+from .views import lead_list, detail_view, lead_create, lead_update
 
 app_name = 'leads'
 
 urlpatterns = [
     path('', lead_list),
     path('<int:pk>/', detail_view),
+    path('<int:pk>/update/', lead_update),
     path('create/', lead_create),
 ]
