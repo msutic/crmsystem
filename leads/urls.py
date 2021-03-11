8edb287dee4289565b5bc2d34d2ce9d6450e4bd1
Marko Sutic <msuticm@gmail.com>
2021-03-12 00:52:23 +0100

    lead delete added

--- leads/urls.py
+++ leads/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import lead_list, detail_view, lead_create, lead_update
+from .views import lead_list, detail_view, lead_create, lead_update, lead_delete
 
 app_name = 'leads'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', lead_list),
     path('<int:pk>/', detail_view),
     path('<int:pk>/update/', lead_update),
+    path('<int:pk>/delete/', lead_delete),
     path('create/', lead_create),
 ]
