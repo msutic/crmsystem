f55879a678103346011ed17fd2a4eac3f343d8d7
Marko Sutic <msuticm@gmail.com>
2021-03-11 02:05:20 +0100

    added a form for creating a new lead

--- leads/urls.py
+++ leads/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import lead_list, detail_view
+from .views import lead_list, detail_view, lead_create
 
 app_name = 'leads'
 
 urlpatterns = [
     path('', lead_list),
-    path('<pk>/', detail_view)
+    path('<int:pk>/', detail_view),
+    path('create/', lead_create),
 ]
